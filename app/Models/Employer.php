ab70a4cd5cf90d4e904bda7272106ad9e47c0f7a
Shadow Walker <franklynpeter2006@gmail.com>
2025-05-28 05:46:12 +0100

    Day 10: Eloquent factory, seeding database with Faker

---
+++ app/Models/Employer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\EmployerFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employer extends Model
+{
+    /** @use HasFactory<EmployerFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+}

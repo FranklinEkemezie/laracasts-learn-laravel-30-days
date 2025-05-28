3b4b83d2343c1e8d1d4961db09f4c00e79b1f398
Shadow Walker <franklynpeter2006@gmail.com>
2025-05-28 06:33:56 +0100

    Day 11: Introduction to Eloquent Relationships (BelongsTo, HasMany)

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Database\Factories\EmployerFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
@@ -14,4 +15,9 @@ class Employer extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
+    }
 }
